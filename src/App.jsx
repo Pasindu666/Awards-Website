e6de7171d1ef39cd17abaf7f4a978079b0c51680
Pasindu Nimsara <pasindu05nimsara@gmail.com>
2024-12-18 22:07:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl font-bold text-violet-300 font-circular-web">
+      Hello world!
+    </h1>
+    </main>
+  )
+}
+
+export default App
